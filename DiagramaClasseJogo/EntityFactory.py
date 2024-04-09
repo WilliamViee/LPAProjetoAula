ecb985238da507b1a46c8c4eb76eacc6e1f4d2af
William Vieira <williamvieira.office@gmail.com>
2024-04-09 04:50:09 -0300

    Signed-off-by: William Vieira <williamvieira.office@gmail.com>

---
+++ DiagramaClasseJogo/EntityFactory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Enemy import Enemy
+
+class EntityFactory(Enemy):
+    def __init__(self):
+        pass
+
+    def get_entity(self, entity_type):
+        pass
+
